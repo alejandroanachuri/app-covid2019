346b51f15e3a6852ef21b914a04b1610d6791e8d
Anachuri <alejandro.anachuri@dxc.com>
2020-08-10 21:13:06 -0300

    New graphic daily info

--- app/main.py
+++ app/main.py
@@ -12,6 +12,10 @@ import requests
 df_countries = pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv',parse_dates=True)
 df_countries_total = df_countries.groupby('Country').max()
 
+df_countries['Confirmed_daily'] = df_countries.groupby('Country')['Confirmed'].diff()
+df_countries['Recovered_daily'] = df_countries.groupby('Country')['Recovered'].diff()
+df_countries['Deaths_daily'] = df_countries.groupby('Country')['Deaths'].diff()
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -22,8 +26,7 @@ server = app.server
 
 top10 = df_countries_total.sort_values(by=['Confirmed'],ascending = False).head(10)
 fig = px.bar(top10, x=top10.index, y=['Confirmed','Deaths','Recovered'])
-#fig = px.bar(top10, x=top10.Country, y='TotalConfirmed', labels={'x':'Country'},
-#             color="TotalConfirmed", color_continuous_scale=px.colors.sequential.Brwnyl)
+
 
 
 app.layout = html.Div(children=[
@@ -49,16 +52,18 @@ app.layout = html.Div(children=[
         style={'width': '48%', 'display': 'inline-block'}
     ),
     dcc.Graph(id='country_graph'),
+    dcc.Graph(id='country_daily_graph'),
+
     html.Footer(children = '© 2020 Powered by Alejandro Anachuri')
 ])
 
 
 @app.callback(
-    Output('country_graph', 'figure'),
+    [Output('country_graph', 'figure'),
+     Output('country_daily_graph', 'figure')],
     [Input('country_dr', 'value')])
 def update_country_graph(country):
     if country == None: return None
-    print(country)
     df_country = df_countries[(df_countries['Country'] == country) & (df_countries['Confirmed']>0)]
 
     line_data = df_country.melt(id_vars='Date', 
@@ -68,9 +73,20 @@ def update_country_graph(country):
                  var_name='Ratio', 
                  value_name='Value')
 
-    figure = px.line(line_data, x="Date", y="Value", line_shape="spline",color='Ratio', 
+    totalfig = px.line(line_data, x="Date", y="Value", line_shape="spline",color='Ratio', 
               title=f'Casos confirmados, casos recuperados y muertes a lo largo del tiempo para {country}')
-    return figure
+
+    line_daily_data = df_country.melt(id_vars='Date', 
+                 value_vars=['Confirmed_daily', 
+                             'Deaths_daily',
+                             'Recovered_daily'], 
+                 var_name='Ratio', 
+                 value_name='Value')
+
+    dailyfig = px.line(line_daily_data, x="Date", y="Value", line_shape="spline",color='Ratio', 
+              title=f'Casos confirmados, casos recuperados y muertes por dia en {country}')
+
+    return totalfig, dailyfig
 
 @server.route('/hello')
 def hello():
